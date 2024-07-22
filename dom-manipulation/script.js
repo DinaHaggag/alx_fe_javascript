86d104efc0b0deacb39367438786f53bfdd023dc
DinaHaggag <dina.m.hagag@gmail.com>
2024-07-23 02:37:55 +0300

    task 0 base

---
+++ dom-manipulation/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded",()=>{
+
+  const newRandomQuote = document.getElementById("newQuote");
+  newRandomQuote.addEventListener("click",showRandomQuote);
+
+  function showRandomQuote (){
+    
+  }
+})
